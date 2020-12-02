3f387c244a1ad29dd5230125b5d6a890baaeb691
Javier Cunat <22517@columbushs.com>
2020-12-02 10:18:21 -0500

    Attempts

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+alert("Game On!!!");
+
 var board = [
     [null, null, null, "S", null],
     [null, "S", null, "S", null ],
@@ -6,21 +9,55 @@ var board = [
     [null, null, null, null, "S"],
    ];
    
-   for (var i = 0; i < 10; i++){
-    var row = getRandomNum();
-    var column = getRandomNum();
+//   for (let i = 0; i < 10; i++){
+//    var row = getRandomNum();
+// var column = getRandomNum();
    
-    var randomSquare = board[row][column];
+//     var randomSquare = board[row][column];
    
-    if (randomSquare === "S"){
-      console.log("Hit on: " + row + ", " + column);
-      board[row][column] = null;
-    }
-    else {
-        console.log("Missed")
-    }
+//    if (randomSquare === "S"){
+//      console.log("Hit on: " + row + ", " + column);
+//      board[row][column] = null;
+//    }
+//    else {
+//      console.log("Missed")
+//    }
+//    }
+   
+//   function getRandomNum(){
+//    return Math.floor(Math.random() * 5);
+//   }
+
+
+
+
+
+
+
+   for (let r=0; r < board.length; r++){
+
+      for (let c=0; c < board[r].length; c++){
+        if (board[r][c] === "S"){
+
+        return 
+         // console.log ('Ship found at: ${r}, ${c}')
+        }
+      }
    }
-   
-   function getRandomNum(){
-    return Math.floor(Math.random() * 5);
+
+   for (let i = 0; i < 10; i++){
+
+      let randRow = Math.floor(Math.random() * board.length );
+
+      let randCol = Math.floor(Math.random() * board[randRow].length);
+
+      let strike = board[randRow][randCol];
+
+      if(strike === 'S'){
+          board[randRow][randCol] = null;
+
+        checkIfWin();
+      
+      }
+
    }
